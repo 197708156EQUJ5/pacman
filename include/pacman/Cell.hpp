2b5f3536c8710299e08ad520d0033d259c0fd43c
197708156EQUJ5 <ggraves73@gmail.com>
2020-11-28 08:41:07 -0500

    separated Cell to separate file

---
+++ include/pacman/Cell.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace pacman
+{
+
+struct Cell
+{
+    int col;
+    int row;
+
+    bool operator==(const Cell& rhs)
+    {
+        return col == rhs.col && row == rhs.row;
+    }
+};
+
+} // namespace pacman
