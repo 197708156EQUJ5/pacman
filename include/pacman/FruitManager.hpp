a167beb1a17865971ce343b4328205f2f23bbddd
197708156EQUJ5 <ggraves73@gmail.com>
2020-12-11 16:14:08 -0500

    all fruits added to array

--- include/pacman/FruitManager.hpp
+++ include/pacman/FruitManager.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 #include "pacman/Cell.hpp"
+#include "pacman/fruit/Apple.hpp"
+#include "pacman/fruit/Bell.hpp"
 #include "pacman/fruit/Cherry.hpp"
 #include "pacman/fruit/Fruit.hpp"
+#include "pacman/fruit/Galaxian.hpp"
+#include "pacman/fruit/Grape.hpp"
+#include "pacman/fruit/Key.hpp"
+#include "pacman/fruit/Peach.hpp"
+#include "pacman/fruit/Strawberry.hpp"
 
 #include <memory>
 #include <vector>
--- src/pacman/FruitManager.cpp
+++ src/pacman/FruitManager.cpp
@@ -7,7 +7,27 @@ namespace pacman
 
 FruitManager::FruitManager()
 {
-    fruits = {std::make_shared<Cherry>()};
+    fruits = {std::make_shared<Cherry>(),
+        std::make_shared<Strawberry>(),
+        std::make_shared<Peach>(),
+        std::make_shared<Peach>(),
+        std::make_shared<Apple>(),
+        std::make_shared<Apple>(),
+        std::make_shared<Grape>(),
+        std::make_shared<Grape>(),
+        std::make_shared<Galaxian>(),
+        std::make_shared<Galaxian>(),
+        std::make_shared<Bell>(),
+        std::make_shared<Bell>(),
+        std::make_shared<Key>(),
+        std::make_shared<Key>(),
+        std::make_shared<Key>(),
+        std::make_shared<Key>(),
+        std::make_shared<Key>(),
+        std::make_shared<Key>(),
+        std::make_shared<Key>(),
+        std::make_shared<Key>(),
+        std::make_shared<Key>()};
 }
 
 Cell FruitManager::getFruit(int level)
